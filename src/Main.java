afb634f9f280cc790ea23e50ddd7a8a483b8c0d2
Marco <marcod3br@gmail.com>
2025-03-27 08:16:27 -0300

    aulabolo

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //Criando o objeto bolo
+        Bolo bolo = new Bolo("Chocolate", 30);
+        bolo.exibirDetalhes();
+    }
+}
